a5353b4213dbe4b5351dac1d41530c1b02259784
unknown <tangtangwzc@gmail.com>
2018-08-07 14:28:12 -0700

    update

--- itracker_adv.py
+++ itracker_adv.py
@@ -685,7 +685,7 @@ def train(args):
 			train_start=iter* chunk_size
 			train_end = (iter+1)* chunk_size
 			batch = load_batch_from_data(train_names, dataset_path, chunk_size, img_ch, img_cols, img_rows, train_start = train_start, train_end = train_end)
-
+			train_data = batch
 			# print (len(batch[0]))
 			# print (np.asarray(batch[0][0]).shape)
 			# print (batch[1].shape)
