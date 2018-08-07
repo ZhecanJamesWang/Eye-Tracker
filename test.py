2138a311573bf5789011fee48380841848eb657c
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-07 12:21:49 -0700

    update test.py

--- test.py
+++ test.py
@@ -50,8 +50,20 @@ for iter in range (int(self.MaxIters)):
 	print (" ------------- iter --------------: ", iter)
 	train_start=iter*self.batch_size
 	train_end = (iter+1)*self.batch_size
-    batch = load_batch_from_data(train_names, dataset_path, batch_size, img_ch, img_cols, img_rows, train_start = train_start, train_end = train_end)
+	batch = load_batch_from_data(train_names, dataset_path, batch_size, img_ch, img_cols, img_rows, train_start = train_start, train_end = train_end)
 
-    print (len(batch[0]))
-    print (np.asarray(batch[0][0]).shape)
-    print (batch[1].shape)
+	print (len(batch[0]))
+	print (np.asarray(batch[0][0]).shape)
+	print (batch[1].shape)
+
+
+
+
+
+
+
+
+
+
+
+# //
