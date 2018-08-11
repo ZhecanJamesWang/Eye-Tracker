a1f6baa817186270fe23dfce731a0fd2a0de455a
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-11 17:40:01 +0800

    update

--- itracker_adv.py
+++ itracker_adv.py
@@ -771,6 +771,15 @@ def train(args):
 				train_data = prepare_data(train_data)
 				val_data = prepare_data(val_data)
 
+
+				print (len(train_data))
+				print (train_data[-5].shape)
+				print (train_data[-4].shape)
+				print (train_data[-3].shape)
+				print (train_data[-2].shape)
+				print (train_data[-1].shape)
+
+				
 				train_loss_history, train_err_history, val_loss_history, val_err_history = et.train(sess, train_data, val_data, \
 														lr = args.learning_rate, \
 														batch_size = args.batch_size, \
