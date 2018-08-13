dcc969ab285e381eb6048a9a0fb5e28951f6c52f
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-13 13:04:31 +0800

    update

--- itracker_adv_orig_data.py
+++ itracker_adv_orig_data.py
@@ -184,36 +184,38 @@ def load_data(file):
 
 	length = len(npzfile["train_eye_left"])
 	print ("length: ", length)
-	index = np.random.randint(0, length - 102)
-	print ("choosing: ", index, " to ", index + 100)
-	train_eye_left = npzfile["train_eye_left"][index : index + 100]
-	train_eye_right = npzfile["train_eye_right"][index : index + 100]
-	train_face = npzfile["train_face"][index : index + 100]
-	train_face_mask = npzfile["train_face_mask"][index : index + 100]
-	train_y = npzfile["train_y"][index : index + 100]
-
-	length = len(npzfile["val_eye_left"])
-	index = np.random.randint(0, length - 102)
+	# index = np.random.randint(0, length - 102)
+	# print ("choosing: ", index, " to ", index + 100)
+	# train_eye_left = npzfile["train_eye_left"][index : index + 100]
+	# train_eye_right = npzfile["train_eye_right"][index : index + 100]
+	# train_face = npzfile["train_face"][index : index + 100]
+	# train_face_mask = npzfile["train_face_mask"][index : index + 100]
+	# train_y = npzfile["train_y"][index : index + 100]
 	#
-	val_eye_left = npzfile["val_eye_left"][index : index + 100]
-	val_eye_right = npzfile["val_eye_right"][index : index + 100]
-	val_face = npzfile["val_face"][index : index + 100]
-	val_face_mask = npzfile["val_face_mask"][index : index + 100]
-	val_y = npzfile["val_y"][index : index + 100]
+	length = len(npzfile["val_eye_left"])
+	print ("length: ", length)
+	# index = np.random.randint(0, length - 102)
+	# #
+	# val_eye_left = npzfile["val_eye_left"][index : index + 100]
+	# val_eye_right = npzfile["val_eye_right"][index : index + 100]
+	# val_face = npzfile["val_face"][index : index + 100]
+	# val_face_mask = npzfile["val_face_mask"][index : index + 100]
+	# val_y = npzfile["val_y"][index : index + 100]
+	#
+	# print ("train_eye_left: ", type(train_eye_left), train_eye_left.shape)
+	# print ("train_y: ", type(train_y), train_y.shape)
 
-	print ("train_eye_left: ", type(train_eye_left), train_eye_left.shape)
-	print ("train_y: ", type(train_y), train_y.shape)
-
-	# train_eye_left = npzfile["train_eye_left"][:100]
-	# train_eye_right = npzfile["train_eye_right"][:100]
-	# train_face = npzfile["train_face"][:100]
-	# train_face_mask = npzfile["train_face_mask"][:100]
-	# train_y = npzfile["train_y"][:100]
-	# val_eye_left = npzfile["val_eye_left"][:100]
-	# val_eye_right = npzfile["val_eye_right"][:100]
-	# val_face = npzfile["val_face"][:100]
-	# val_face_mask = npzfile["val_face_mask"][:100]
-	# val_y = npzfile["val_y"][:100]
+	limit = 1000
+	train_eye_left = npzfile["train_eye_left"][:limit]
+	train_eye_right = npzfile["train_eye_right"][:limit]
+	train_face = npzfile["train_face"][:limit]
+	train_face_mask = npzfile["train_face_mask"][:limit]
+	train_y = npzfile["train_y"][:limit]
+	val_eye_left = npzfile["val_eye_left"][:limit]
+	val_eye_right = npzfile["val_eye_right"][:limit]
+	val_face = npzfile["val_face"][:limit]
+	val_face_mask = npzfile["val_face_mask"][:limit]
+	val_y = npzfile["val_y"][:limit]
 
 	# print ("train_eye_left: ", type(train_eye_left), train_eye_left.shape)
 	# print ("train_y: ", type(train_y), train_y.shape)
