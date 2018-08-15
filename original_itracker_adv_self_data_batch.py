910fd02d2e8a5882d81c8ad3fea43c0aa84c8c1f
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-15 21:27:26 +0800

    update

--- original_itracker_adv_self_data_batch.py
+++ original_itracker_adv_self_data_batch.py
@@ -578,7 +578,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--train', action='store_true', help='train flag')
 	# parser.add_argument('-i', '--input', required=True, type=str, help='path to the input data')
-	parser.add_argument('-max_epoch', '--max_epoch', type=int, default=100, help='max number of iterations')
+	parser.add_argument('-max_epoch', '--max_epoch', type=int, default=3, help='max number of iterations')
 	parser.add_argument('-lr', '--learning_rate', type=float, default=0.0025, help='learning rate')
 	parser.add_argument('-bs', '--batch_size', type=int, default=200, help='batch size')
 	parser.add_argument('-p', '--patience', type=int, default=np.Inf, help='early stopping patience')
