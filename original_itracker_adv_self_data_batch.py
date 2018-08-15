182392fca318e82f82d120b56edab23cbab2ac95
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2018-08-15 18:28:03 +0800

    update

--- original_itracker_adv_self_data_batch.py
+++ original_itracker_adv_self_data_batch.py
@@ -252,9 +252,11 @@ class EyeTracker(object):
 	def train(self, lr=1e-3, batch_size=128, max_epoch=1000, min_delta=1e-4, patience=10, print_per_epoch=10, out_model='my_model'):
 		# train_data, val_data,
 
-		limit = np.Inf
-		train_names = load_data_names(train_path)[:limit]
-		val_names = load_data_names(val_path)[:limit]
+		# limit = 1000
+		train_names = load_data_names(train_path)
+		# [:limit]
+		val_names = load_data_names(val_path)
+		# [:limit]
 
 		train_num = len(train_names)
 		val_num = len(val_names)
