06dedf3d56e5b658a55c2c945f19c9aa75d038de
zhecan wang <zhecanwang@zhecans-MacBook-Pro-2.local>
2018-09-19 23:19:14 +0800

    update

--- original_itracker_adv_multi_task.py
+++ original_itracker_adv_multi_task.py
@@ -2,7 +2,7 @@ import argparse
 import timeit
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from load_data import load_data_names, load_batch_from_data, load_data_mpii, load_data_names_columbia, load_batch_from_data_columbia
 import datetime
 import random
